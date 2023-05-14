b79cb27754b5031158c43161e1431bfeb7a8daee
matekuna92 <kunamate66@gmail.com>
2023-05-14 22:41:14 +0200

    manifest icon size fix, npm start legacy fix for older project, basic modal toggle

--- src/App.js
+++ src/App.js
@@ -6,13 +6,25 @@ import Backdrop from "./components/Backdrop/Backdrop";
 import List from "./components/List/List";
 
 class App extends Component {
+  state = {
+    isModalOpen: false
+  };
+
+  showModal = () => {
+    this.setState({isModalOpen: true});
+  }
+
+ closeModal = () => {
+    this.setState({isModalOpen: false});
+  }
+
   render() {
     return (
       <div className="App">
         <h1>React Animations</h1>
-        <Modal />
-        <Backdrop />
-        <button className="Button">Open Modal</button>
+        <Modal closed={this.closeModal} show={this.state.isModalOpen} />
+        <Backdrop show={this.state.isModalOpen} />
+        <button className="Button" onClick={this.showModal}>Open Modal</button>
         <h3>Animating Lists</h3>
         <List />
       </div>
--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -2,11 +2,13 @@ import React from 'react';
 
 import './Modal.css';
 
-const modal = (props) => (
-    <div className="Modal">
+const modal = (props) => {
+    const classes = `Modal ${props.show ? 'ModalOpen' : 'ModalClosed'}`;
+
+    return <div className={classes}>
         <h1>A Modal</h1>
         <button className="Button" onClick={props.closed}>Dismiss</button>
     </div>
-);
+}
 
 export default modal;
