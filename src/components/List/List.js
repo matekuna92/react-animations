ce006191e61d07568cac43348136a83d1d95401f
matekuna92 <kunamate66@gmail.com>
2023-05-30 15:47:47 +0200

    CSSTransition with TransitionGroup

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { TransitionGroup, CSSTransition } from 'react-transition-group';
 
 import './List.css';
 
@@ -23,21 +24,29 @@ class List extends Component {
         });
     }
 
+    // TransitionGroup can only be used together with Transition or CSSTransition.
+    // without it app gets multiply errors:
+    // Warning: Unknown event handler property `onExited`. It will be ignored.
+    // Warning: Received `true` for a non-boolean attribute `in`.
+    
+    // TransitionGroup manages the "in" property automatically for dynamic lists to control the state of transition
+    // it determines if an element is added or removed and automatically set the in property
     render () {
         const listItems = this.state.items.map( (item, index) => (
-            <li 
-                key={index}
-                className="ListItem" 
-                onClick={() => this.removeItemHandler(index)}>{item}</li>
-        ) );
+            <CSSTransition key={index} classNames='fade' timeout={300}>
+                <li 
+                    className="ListItem" 
+                    onClick={() => this.removeItemHandler(index)}>{item}</li>
+            </CSSTransition>
+        ));
 
         return (
             <div>
                 <button className="Button" onClick={this.addItemHandler}>Add Item</button>
                 <p>Click Item to Remove.</p>
-                <ul className="List">
+                <TransitionGroup component='ul' className='List'>
                     {listItems}
-                </ul>
+                </TransitionGroup>
             </div>
         );
     }
